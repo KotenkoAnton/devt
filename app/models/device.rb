6353d50fc6f3c04694754e1ed72cec878e14cff0
Kotenko Anton <mainkotenkoanton@gmail.com>
2018-10-23 10:24:44 +0300

    Add validations.

--- app/models/device.rb
+++ app/models/device.rb
@@ -1,3 +1,5 @@
 class Device < ActiveRecord::Base
   has_one :item, as: :placeable
+
+  validates_uniqueness_of :ip_address
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -2,4 +2,6 @@ class Item < ActiveRecord::Base
   belongs_to :placeable, polymorphic: true
   has_one :connection, as: :connectable
   belongs_to :map
+
+  validates_uniqueness_of :name, scope: :map_id
 end
