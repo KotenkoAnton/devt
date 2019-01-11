8536cfe16ec22bdc0de1147895677b8a3d035ae4
Kotenko Anton <mainkotenkoanton@gmail.com>
2019-01-11 08:49:56 +0300

    Add MapCorrector.

---
+++ lib/map_corrector.rb
@@ -0,0 +1,36 @@
+module MapCorrector
+  class << self
+    INDENTATION = 100
+
+    def correct_items_position
+      Map.all.each do |map|
+        correct_on_map(map)
+      end
+    end
+
+    def correct_on_map(map)
+      items = Item.where(map: map)
+      shapes = Shape.where(map: map)
+      return if (items + shapes).empty?
+
+      min_x = (items + shapes).min_by(&:position_x).position_x
+      min_y = (items + shapes).min_by(&:position_y).position_y
+
+      move_by({ x: INDENTATION - min_x, y: INDENTATION - min_y }, map)
+    end
+
+    def move_by(shift, map)
+      Item.where(map: map).each do |item|
+        item.position_x += shift[:x]
+        item.position_y += shift[:y]
+        item.save
+      end
+
+      Shape.where(map: map).each do |shape|
+        shape.position_x += shift[:x]
+        shape.position_y += shift[:y]
+        shape.save
+      end
+    end
+  end
+end
