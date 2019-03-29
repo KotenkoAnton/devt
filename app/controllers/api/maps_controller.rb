1776d358008b4a9078f6021c70bf5b71b8a9ed25
Kotenko Anton <mainkotenkoanton@gmail.com>
2019-03-29 10:17:33 +0300

    Improve list view.

--- app/controllers/api/maps_controller.rb
+++ app/controllers/api/maps_controller.rb
@@ -45,7 +45,8 @@ module Api
       devices = items.map do |item|
         { ip: item.placeable.ip_address.ip_address, icmp_available: item.placeable.ip_address.icmp_available,
           name: item.name, type: item.placeable.host_type_name,
-          changed_status_at: item.placeable.ip_address[:changed_status_at]&.strftime("%d/%m/%y %H:%M:%S") }
+          changed_status_at: item.placeable.ip_address[:changed_status_at]&.strftime("%d/%m/%y %H:%M:%S"),
+          item_id: item.id }
       end
       render json: { devices: devices }
     end
